6725af56980546afdb56745daa5df38a026695b6
YamaKan <yamakan.67@gmail.com>
2016-10-09 10:43:14 +0900

    template.py

---
+++ python/template.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+
+    for arg in args :
+        print u"args {}".format(arg)
+
+    sys.exit(0);
